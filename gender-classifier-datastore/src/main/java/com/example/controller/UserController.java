8e6dfe9da7f4154dd17b6a51653da3d8b0b8ed9e
Mayank-141-Shaw <mayank141shaw@gmail.com>
2022-07-12 00:28:50 +0530

    Adding the controllers and entities to the repo

---
+++ gender-classifier-datastore/src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.User;
+import com.example.service.UserService;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	
+	/** 
+	 * The function would parse id from the query request and
+	 * return the user of the requested id
+	 * If it does not exist then a null object shall be returned
+	 * 
+	 * @param	id		Long
+	 * @return	User	User object or null
+	 * @see 			getUserData
+	 */
+	
+	@GetMapping("/id/{id}")
+	public User getUserData(@PathVariable long id) {
+		return userService.getUserById(id);
+	}
+}
